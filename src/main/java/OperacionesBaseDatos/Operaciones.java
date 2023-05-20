65b7600442ff468b6b8395dfb32417c324b46804
Alex_Informatic <alexanderadell2003@gmail.com>
2023-05-20 13:10:02 +0200

    Subida de paquetes

---
+++ src/main/java/OperacionesBaseDatos/Operaciones.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package OperacionesBaseDatos;
+
+/**
+ *
+ * @author 34606
+ */
+public class Operaciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
