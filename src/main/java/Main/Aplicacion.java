f168c973a07f9f6a387b04141411de13188eb745
Alex_Informatic <alexanderadell2003@gmail.com>
2023-06-04 18:19:44 +0200

    Subida de paquetes

--- src/main/java/Main/Aplicacion.java
+++ src/main/java/Main/Aplicacion.java
@@ -1,19 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
 package Main;
 
-/**
- *
- * @author 34606
- */
+import Vistas.VistaGF;
+
 public class Aplicacion {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
-        // TODO code application logic here
+        VistaGF vista = new VistaGF();
     }
 }
